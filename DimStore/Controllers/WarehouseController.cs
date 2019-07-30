cf3a11f7efe4f6dbf4c74ab6b1cf0e412659ecfd
DavidIMk <davidimmanuelmaksi@gmail.com>
2019-07-30 21:16:17 +0700

    initial commit

---
+++ DimStore/Controllers/WarehouseController.cs
@@ -0,0 +1,214 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DimStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DimStore.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class WarehouseController : ControllerBase
+    {
+        protected readonly ILogger Logger;
+        protected readonly StockItemsConfiguration.WideWorldImportersDbContext DbContext;
+
+        public WarehouseController(ILogger<WarehouseController> logger, StockItemsConfiguration.WideWorldImportersDbContext dbContext)
+        {
+            Logger = logger;
+            DbContext = dbContext;
+        }
+
+        [HttpGet("StockItem")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> GetStockItemsAsync(int pageSize = 10, int pageNumber = 1, int? lastEditedBy = null, 
+            int? colorID = null, int? outerPackageID = null, int? supplierID = null, int? unitPackageID = null)
+        {
+            Logger?.LogDebug("'{0}' has been invoked", nameof(GetStockItemsAsync));
+
+            var response = new PagedResponse<StockItem>();
+
+            try
+            {
+                // Get the "proposed" query from repository
+                var query = DbContext.GetStockItems();
+
+                // Set paging values
+                response.PageSize = pageSize;
+                response.PageNumber = pageNumber;
+
+                // Get total rows
+                response.ItemsCount = await query.CountAsync();
+
+                // Get specific page from database
+                response.Model = await query.Paging(pageSize, pageNumber).ToListAsync();
+
+                response.Message = string.Format("Page {0} of {1}, Total of products: {2}.", pageNumber, response.PageCount, response.ItemsCount);
+
+                Logger.LogInformation("The stock items have been retrieved successfully.");
+            }
+            catch(Exception ex)
+            {
+                response.DidError = true;
+                response.ErrorMessage = "There was an internal error, please contact to technical support.";
+
+                Logger?.LogCritical("There was an error on '{0}' invocation: {1}", nameof(GetStockItemsAsync), ex);
+            }
+            return response.ToHttpResponse();
+        }
+
+        [HttpGet("StockItem/{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> GetStockItemAsync(int id)
+        {
+            Logger?.LogDebug("'{0}' has been invoked", nameof(GetStockItemAsync));
+
+            var response = new SingleResponse<StockItem>();
+
+            try
+            {
+                // Get the stock item by id
+                response.Model = await DbContext.GetStockItemsAsync(new StockItem(id));
+            }
+            catch (Exception ex)
+            {
+                response.DidError = true;
+                response.ErrorMessage = "There was an internal error, please contact to technical support.";
+
+                Logger?.LogCritical("There was an error on '{0}' invocation: {1}", nameof(GetStockItemAsync), ex);
+            }
+
+            return response.ToHttpResponse();
+        }
+
+        [HttpPost("StockItem")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> PostStockItemAsync([FromBody]PostStockItemsRequest request)
+        {
+            Logger?.LogDebug("'{0}' has been invoked", nameof(PostStockItemAsync));
+
+            var response = new SingleResponse<StockItem>();
+
+            try
+            {
+                var existingEntity = await DbContext.GetStockItemsByStockItemNameAsync(new StockItem { StockItemName = request.StockItemName });
+                
+                if(existingEntity != null)
+                {
+                    ModelState.AddModelError("StockItemName", "Stock item name already exists");
+                }
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+
+                // Create Entity from request
+                var entity = request.ToEntity();
+
+                // Add entity to repo
+                DbContext.Add(entity);
+
+                // Save entity in database
+                await DbContext.SaveChangesAsync();
+
+                // Set the entity to response model
+                response.Model = entity;
+            }
+            catch(Exception ex)
+            {
+                response.DidError = true;
+                response.ErrorMessage = "There was an internal error, please contact to technical support.";
+
+                Logger?.LogCritical("There was an error on '{0}' invocation: {1}", nameof(PostStockItemAsync), ex);
+            }
+
+            return response.ToHttpResponse();
+        }
+
+        [HttpPut("StockItem/{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> PutStockItemAsync(int id, [FromBody]PutStockItemsRequest request)
+        {
+            Logger?.LogDebug("'{0}' has been invoked", nameof(PutStockItemAsync));
+
+            var response = new Response();
+
+            try
+            {
+                // Get stock item by id
+                var entity = await DbContext.GetStockItemsAsync(new StockItem(id));
+
+                // Validate if entity exists
+                if (entity == null)
+                    return NotFound();
+
+                // Set changes to entity
+                entity.StockItemName = request.StockItemName;
+                entity.SupplierID = request.SupplierID;
+                entity.ColorID = request.ColorID;
+                entity.UnitPrice = request.UnitPrice;
+
+                // Update entity in repository
+                DbContext.Update(entity);
+
+                // Save entity in database
+                await DbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                response.DidError = true;
+                response.ErrorMessage = "There was an internal error, please contact to technical support.";
+
+                Logger?.LogCritical("There was an error on '{0}' invocation: {1}", nameof(PutStockItemAsync), ex);
+            }
+
+            return response.ToHttpResponse();
+        }
+
+        [HttpDelete("StockItem/{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> DeleteStockItemAsync(int id)
+        {
+            Logger?.LogDebug("'{0}' has been invoked", nameof(DeleteStockItemAsync));
+
+            var response = new Response();
+
+            try
+            {
+                // Get stock item by id
+                var entity = await DbContext.GetStockItemsAsync(new StockItem(id));
+
+                // Validate if entity exists
+                if (entity == null)
+                    return NotFound();
+
+                // Remove entity from repository
+                DbContext.Remove(entity);
+
+                // Delete entity in database
+                await DbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                response.DidError = true;
+                response.ErrorMessage = "There was an internal error, please contact to technical support.";
+
+                Logger?.LogCritical("There was an error on '{0}' invocation: {1}", nameof(DeleteStockItemAsync), ex);
+            }
+
+            return response.ToHttpResponse();
+        }
+    }
+}
